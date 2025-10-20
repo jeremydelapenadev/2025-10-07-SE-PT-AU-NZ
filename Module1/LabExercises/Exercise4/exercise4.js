0b2dfe9ca1db218d30e16685f5614d1207ec7733
jeremydelapenadev <jeremydelapena.dev@gmail.com>
2025-10-21 10:55:39 +1100

    Completed the Lab 4 Exercise, with formulas and name

---
+++ Module1/LabExercises/Exercise4/exercise4.js
@@ -0,0 +1,26 @@
+function Sum(a, b) {
+    return a + b;
+}
+function Subtract(a, b) {
+    return a - b;
+}
+function Multiply(a, b) {
+    return a * b;
+}
+function Divide(a, b) {
+    return a / b;
+}
+console.log(Multiply(Sum(2, 3), Sum(11, 5)));
+console.log(Sum(5,5));
+console.log(Divide(10,2));
+console.log(Subtract(765,321));
+
+// "Hello, <name>""
+let firstName = "Jeremy";
+let lastName = "Dela Pena";
+
+// Concatenate using the + operator
+let fullName = "Hello, " + firstName + " " + lastName;
+
+// Print the result
+console.log(fullName);
